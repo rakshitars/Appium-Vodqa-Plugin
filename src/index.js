5ff664757cbf0131844de199b079c8236cdd5cce
saikrishna321 <saikrishna321@yahoo.com>
2022-08-25 18:08:57 +0530

    initial commit

---
+++ src/index.js
@@ -0,0 +1,4 @@
+import VodqaDemoPlugin from './plugin'
+
+export default VodqaDemoPlugin;
+export {VodqaDemoPlugin}
---
+++ src/plugin.js
@@ -0,0 +1,5 @@
+import { BasePlugin } from "appium/plugin";
+
+export default class VodqaDemoPlugin extends BasePlugin {
+    
+}
